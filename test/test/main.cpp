057e04a879e83e54cb1c7275ef838668c39db439
jahongir20030601 <103406851+jahongir20030601@users.noreply.github.com>
2022-04-28 14:42:10 +0500

    Xcode project was added
    We simply opened another xcode project within the cloned repo's directory

---
+++ test/test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by JAKHONGIR BAKHODIROV on 28/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
